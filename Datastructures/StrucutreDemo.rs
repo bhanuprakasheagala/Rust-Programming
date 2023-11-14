81024db9674137f11e42661edc15deefac4050fc
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2023-11-14 07:50:07 +0530

    Struct Intro: version2

--- Datastructures/StrucutreDemo.rs
+++ Datastructures/StrucutreDemo.rs
@@ -3,6 +3,7 @@
 struct Person {
     first_name: String,
     last_name: String,
+    age: u8,
 }
 
 fn main(){
@@ -10,10 +11,13 @@ fn main(){
     let first_name = read_string();
     println!("What is your last name?");
     let last_name = read_string();
+    println!("What is your Age?");
+    let age = read_number();
 
     let person = Person {
         first_name: first_name,
         last_name: last_name,
+        age: age,
     };
 
     print_person(&person);
@@ -29,6 +33,21 @@ fn read_string() -> String {
     cleaned_input
 }
 
+fn read_number() -> u8 {
+    let mut input = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("failed to read input!");
+    let input: u8 = input.trim().parse().expect("Not a positive number");
+
+    input
+}
+
+/*
+Note that we lend our person to the print_person function with the & operator. The & must be added to the signature of the print_person
+function which os taking a &Person as input, as well as the calling function that is passing &person.
+*/
 fn print_person(person: &Person){
     println!("Hello {} {}", person.first_name, person.last_name);
+    println!("Your Age: {}", person.age);
 }
